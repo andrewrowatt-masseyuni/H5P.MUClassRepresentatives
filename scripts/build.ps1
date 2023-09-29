09ed089dcbaa9c9fd0dc96f30f6feb1ab60f994b
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2023-09-30 07:31:39 +1300

    Minor updates

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,4 @@
 Set-Location -Path 'scripts'
 Remove-Item -Path 'h5p.h5p'
-Get-ChildItem -Path .. -Exclude *.zip,*.h5p,README.md,scripts,.vscode | Compress-Archive -DestinationPath 'h5p.zip' -Force
+Get-ChildItem -Path .. -Exclude *.zip,*.h5p,README.md,scripts,.vscode | Compress-Archive -DestinationPath 'h5p.zip' -Force -CompressionLevel NoCompression
 Rename-Item -Path 'h5p.zip' -NewName 'h5p.h5p'
